90f67bd41902f54469d8c9a4a21ea52f954df78b
Tristis <henhov112@gmail.com>
2026-02-28 10:22:21 +0100

    progress bar added to simulation

--- Interference_simulation.py
+++ Interference_simulation.py
@@ -3,9 +3,10 @@ import scipy.constants as const
 import matplotlib.animation as ani
 import matplotlib.pyplot as plt
 from numba import njit
+from numba_progress import ProgressBar
 
-@njit
-def simulation():
+@njit(nogil = True)
+def simulation(numba_prog):
     n_h2o = const.c / 4 #1.33
     n_air = const.c / 1.00029
 
@@ -16,8 +17,7 @@ def simulation():
     x_min = y_min = -10
     x_max = y_max = 10
 
-    N_x = 200
-    N_y = 200
+    N_x = N_y = 500
 
     h = (x_max - x_min) / N_x
     dt = 1e-10
@@ -38,7 +38,7 @@ def simulation():
 
 
     def wave_origin(t:np.float64):
-        return np.sin(5e06 * t) * 20
+        return np.sin(1e04 * t) * 200
 
     def update_meshgrid(x:int, y:int, u_1:np.ndarray, u_2:np.ndarray):
         a = alpha[x][y]
@@ -49,12 +49,13 @@ def simulation():
 
     for i in x_index:
         for j in y_index[0:10]:
-            u_2[np.where(x_index == i)[0][0]][np.where(y_index == j)[0][0]] = 20
-            u_1[np.where(x_index == i)[0][0]][np.where(y_index == j)[0][0]] = 20
+            u_2[np.where(x_index == i)[0][0]][np.where(y_index == j)[0][0]] = 200
+            u_1[np.where(x_index == i)[0][0]][np.where(y_index == j)[0][0]] = 200
 
     u = np.zeros((N_x, N_y), dtype = np.float64)
 
     for t in np.arange(t_min, t_max, dt, dtype = np.float64):
+        numba_prog.update(1)
         for i in x_index[1:-1]:
             for j in y_index[1:-1]:
                 x = np.where(x_index == i)[0][0]
@@ -72,7 +73,12 @@ def simulation():
         u = np.zeros((N_x, N_y), dtype = np.float64)
     return u_db
 
-u_db = simulation()
+t_min = 0
+t_max = 1e-07
+dt = 1e-10
+
+with ProgressBar(total = len(np.arange(t_min, t_max, dt)), ncols = 80) as numba_prog_1:
+    u_db = simulation(numba_prog_1)
 
 fig, axs = plt.subplots()
 
@@ -82,5 +88,6 @@ def update(frame):
     im.set_array(u_db[frame])
     return im,
 
-ani = ani.FuncAnimation(fig, update, interval=50, blit=True, frames = len(u_db))
+anim = ani.FuncAnimation(fig, update, interval=50, blit=True, frames = len(u_db))
+anim.save("Wave_sim_v4.gif", fps = 10, writer = "PillowWriter")
 plt.show()
